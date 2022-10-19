f082b153816dbc15849a525cd29240ea87503618
John Barnes VI <lj502jr@gmail.com>
2022-10-19 00:04:33 -0500

    added app element

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React from "react"
 
 function App() {
-
-  const [backendData, setBackendData] = useState([{}])
-
-  useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, [])
-
   return (
     <div>
-
-    {(typeof backendData.recipes === 'undefined') ? (
-      <p>Loading...</p>
-    ): (
-      backendData.recipes.map((recipe, i) => (
-        <p key={i}>{recipe}</p>
-      ))
-    )}
-
+      <p>
+        Hello World!
+      </p>
     </div>
   )
 }
---
+++ client/src/GetData.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+function GetData() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
+  return (
+    <div>
+
+    {(typeof backendData.recipes === 'undefined') ? (
+      <p>Loading...</p>
+    ): (
+      backendData.recipes.map((recipe, i) => (
+        <p key={i}>{recipe}</p>
+      ))
+    )}
+
+    </div>
+  )
+}
+
+export default GetData
