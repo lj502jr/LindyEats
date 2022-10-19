a09ac852daf85c4ae36d815465d87c454436f466
John Barnes VI <lj502jr@gmail.com>
2022-10-18 19:22:51 -0500

    server and client can now talk to each other

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express()
 
 app.get("/api", (req, res) => {
-  res.json( {"users": ["userOne", "userTwo", "userThree"] })
+  res.json( {"users": ["userOne", "userTwo", "userThree", "userFour"] })
 })
 
 app.listen(5050, () => { console.log("Server started on port 5050") })
