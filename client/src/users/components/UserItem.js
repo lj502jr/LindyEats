a9715455b39accdce53b33cd5da5e9cabf50e937
John Barnes VI <lj502jr@gmail.com>
2022-10-20 15:01:04 -0500

    user styling working

--- client/src/users/components/UserItem.js
+++ client/src/users/components/UserItem.js
@@ -1,21 +1,27 @@
-import React from "react"
-import './UserItem.css'
-import Avatar from './../../shared/components/UIElements/Avatar'
+import React from 'react';
+import { Link } from "react-router-dom";
+import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
+import "./UserItem.css";
+
+
 
 const UserItem = props => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <Avatar image={props.image} alt={props.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.commentCount} {props.commentCount === 1 ? "Comment" : "Comments"}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${props.id}/comments`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.commentCount} {props.commentCount === 1 ? "Comment" : "Comments"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   )
 }
--- client/src/users/pages/Users.js
+++ client/src/users/pages/Users.js
@@ -8,36 +8,16 @@ const Users = () => {
       name: 'John Barnes',
       image: 'https://forums.steamrep.com/data/avatars/l/93/93969.jpg?1436795071',
       comments: 3
-    },
-    {
-      id: 'u2',
-      name: 'Bill Franklin',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_SkrmLf5uU2v1A9be2G_BBEXcESux_qOTyA&usqp=CAU',
-      comments: 1
-    },
-    {
-      id: 'u3',
-      name: 'Jake Brumbaugh',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_SkrmLf5uU2v1A9be2G_BBEXcESux_qOTyA&usqp=CAU',
-      comments: 1
-    },
-    {
-      id: 'u4',
-      name: 'Paige Barnes',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_SkrmLf5uU2v1A9be2G_BBEXcESux_qOTyA&usqp=CAU',
-      comments: 1
-    },
-    {
-      id: 'u5',
-      name: 'Jack Paynter',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_SkrmLf5uU2v1A9be2G_BBEXcESux_qOTyA&usqp=CAU',
-      comments: 1
-    },
-    {
-      id: 'u6',
-      name: 'William Judd',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_SkrmLf5uU2v1A9be2G_BBEXcESux_qOTyA&usqp=CAU',
-      comments: 1
+    },{
+      id: 'u1',
+      name: 'John Barnes',
+      image: 'https://forums.steamrep.com/data/avatars/l/93/93969.jpg?1436795071',
+      comments: 3
+    },{
+      id: 'u1',
+      name: 'John Barnes',
+      image: 'https://forums.steamrep.com/data/avatars/l/93/93969.jpg?1436795071',
+      comments: 3
     }
   ]
 
