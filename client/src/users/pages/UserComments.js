a60165084286532897df2ac2609ca3afde27ac8a
John Barnes VI <lj502jr@gmail.com>
2022-10-20 22:35:06 -0500

    Make comments page look better

--- client/src/users/pages/UserComments.js
+++ client/src/users/pages/UserComments.js
@@ -12,18 +12,60 @@ const DUMMY_COMMENTS = [
     content: 'Wow what a nice website'
   },
   {
-    id: 'c1',
+    id: 'c2',
     user: 'John Barnes',
     userId: 'u1',
     userImg: 'https://forums.steamrep.com/data/avatars/l/93/93969.jpg?1436795071',
     content: 'Giggity'
   },
   {
-    id: 'c1',
+    id: 'c3',
     user: 'John Barnes',
     userId: 'u1',
     userImg: 'https://forums.steamrep.com/data/avatars/l/93/93969.jpg?1436795071',
     content: 'Shooo-weee'
+  },
+  {
+    id: 'c4',
+    user: 'Paige Barnes',
+    userId: 'u2',
+    userImg: 'https://m.media-amazon.com/images/I/31mxcsDxJqL.jpg',
+    content: 'I\'m Paige'
+  },
+  {
+    id: 'c5',
+    user: 'Jill Barnes',
+    userId: 'u3',
+    userImg: 'https://i.pinimg.com/236x/b4/f1/f1/b4f1f19330a0542e69f1ea8c92ced4fc.jpg',
+    content: 'I\'m Jill'
+  },
+  {
+    id: 'c6',
+    user: 'Jill Barnes',
+    userId: 'u3',
+    userImg: 'https://i.pinimg.com/236x/b4/f1/f1/b4f1f19330a0542e69f1ea8c92ced4fc.jpg',
+    content: 'Shooo-weee these grits are good'
+  },
+  {
+    id: 'c7',
+    user: 'Jill Barnes',
+    userId: 'u3',
+    userImg: 'https://i.pinimg.com/236x/b4/f1/f1/b4f1f19330a0542e69f1ea8c92ced4fc.jpg',
+    content: 'I like schnitzel'
+  },
+  {
+    id: 'c8',
+    user: 'Jill Barnes',
+    userId: 'u3',
+    userImg: 'https://i.pinimg.com/236x/b4/f1/f1/b4f1f19330a0542e69f1ea8c92ced4fc.jpg',
+    content: 'I like Dateline'
+  },
+  {
+    id: 'c9',
+    user: 'Jill Barnes',
+    userId: 'u3',
+    userImg: 'https://i.pinimg.com/236x/b4/f1/f1/b4f1f19330a0542e69f1ea8c92ced4fc.jpg',
+    content: 'We should make more tacos'
   }
 ]
 
--- client/src/users/pages/Users.js
+++ client/src/users/pages/Users.js
@@ -9,15 +9,15 @@ const Users = () => {
       image: 'https://forums.steamrep.com/data/avatars/l/93/93969.jpg?1436795071',
       comments: 3
     },{
-      id: 'u1',
-      name: 'John Barnes',
-      image: 'https://forums.steamrep.com/data/avatars/l/93/93969.jpg?1436795071',
-      comments: 3
+      id: 'u2',
+      name: 'Paige Barnes',
+      image: 'https://m.media-amazon.com/images/I/31mxcsDxJqL.jpg',
+      comments: 1
     },{
-      id: 'u1',
-      name: 'John Barnes',
-      image: 'https://forums.steamrep.com/data/avatars/l/93/93969.jpg?1436795071',
-      comments: 3
+      id: 'u3',
+      name: 'Jill Barnes',
+      image: 'https://i.pinimg.com/236x/b4/f1/f1/b4f1f19330a0542e69f1ea8c92ced4fc.jpg',
+      comments: 5
     }
   ]
 
